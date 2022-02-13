4b4087f981a7dbb7a257504774b5321b457c9d66
Moffett <20ninjas@gmail.com>
2022-02-13 10:32:58 -0800

    remove node rendering

--- src/pages/simulation/SimulationViewer.jsx
+++ src/pages/simulation/SimulationViewer.jsx
@@ -73,64 +73,6 @@ const UpdatedMap = (props) => {
             height={canvasHeight}/>
         </div>
     )
-
-    return (
-        <div style={{marginTop: '20px', display: 'grid', fontSize: 'small'}}>   {/* entire board */}
-            {
-                props.simboard.map(function (row, rowId) {
-                    return (
-                        <div style={{justifyContent: 'center',
-                                    lineHeight: '0',}}
-                             key={rowId}>                                       {/* each row*/}
-                            {
-                                row.map((col, colId) => {                       {/* each col*/}
-
-                                    let opacityVal = col.strength / 100;
-                                    let backgroundColor = 'rgba(' + col.color.r + ',' + col.color.g + ',' + col.color.b + ',' + opacityVal + ')'
-                                    let backgroundInfoColor = 'rgba(' + col.color.r + ',' + col.color.g + ',' + col.color.b + ',' + 0.8 + ')'
-
-                                    return ( 
-                                        <OverlayTrigger
-                                            key={col.id + "overlay"}
-                                            placement="right"
-                                            delay={{ show: 250, hide: 400 }}
-                                            overlay={
-                                                <div 
-                                                    className="rounded border border-primary"
-                                                    style={{ 
-                                                        background: 'lightGray', 
-                                                        fontSize: 'smaller',
-                                                        padding: '4px',
-                                                        // borderRadius: '3px',
-                                                        // width: '10%',
-                                                        // height: '10%', 
-                                                    }}> 
-                                                        {"Id: " + col.id}<br></br>
-                                                        {"Name: " + col.name}<br></br> 
-                                                        {"Pos: " + col.position.x + " " + col.position.y}<br></br>
-                                                        {"Str: " + col.strength}<br></br>
-                                                </div>
-                                            }>
- 
-                                        <div    className="rounded border border-dark"
-                                                key={col.id} 
-                                                // xs={{span: 1, offset: 0}}
-                                                style={{display: 'inline-block', 
-                                                        background: backgroundColor, 
-                                                        width: '10px',
-                                                        height: '10px', 
-                                                }}>
-                                        </div>
-                                        </OverlayTrigger>
-                                    );
-                                }) //end row.map
-                            }       
-                        </div>
-                    );
-                }) //end simboard.map
-            }    
-        </div>       
-    );
 }
   
 export {
